05750223c52de8b2cf2bf13046b3eb4d85703eb4
danie <danie@Evil_Machine>
2025-11-23 19:48:42 -0600

    formulariotelefono

--- Agenda/src/main/java/com/mycompany/agenda/ConsultaGeneral.java
+++ Agenda/src/main/java/com/mycompany/agenda/ConsultaGeneral.java
@@ -110,6 +110,8 @@ public class ConsultaGeneral extends javax.swing.JFrame {
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
         this.dispose();
+        MenuPrincipal principal = new MenuPrincipal();
+        principal.setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
